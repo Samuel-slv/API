e024290aebbdf1017551d9f648ec69ac8485e388
Samuel da Silva <samuelsilva2017ramos@gmail.com>
2025-04-02 10:32:18 -0300

    update

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 app.use(express.json());
 
+const users = []
+
+//Criar dados / ususarios / fazer requeisição POST
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    res.status(201).json(users);
+})
+
+//Mostrar /dados / usuarios / fazer requeisição GET
+
 app.get('/users', (req, res) => {
     //res.status(200).json(users);
-    res.send('ok, I am here.');
+    res.status(200).json(users);
 });
 
 //ip 127.0.0.1:8080*/
