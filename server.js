992fb9d0c3b42fa61a8281b49384277db373d9cd
Samuel da Silva <samuelsilva2017ramos@gmail.com>
2025-04-03 11:15:54 -0300

    update de files 03/04

--- server.js
+++ server.js
@@ -1,13 +1,70 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
+
+const app = express();
+app.use(express.json());
+const users = []
+
+//Criar dados / ususarios / fazer requeisição POST
+app.post('/users', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
+
+    users.push(req.body);
+    res.status(201).json(users);
+})
+
+//Mostrar /dados / usuarios / fazer requeisição GET
+
+app.get('/users', async (req, res) => {
+
+    const users = prisma.user.findMany();
+    res.status(200).json(users);
+});
+
+//ip 127.0.0.1:8082
+app.listen(8082);
+
+
+
+
+
+
+
+
+
+/* import express from 'express';
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
 const users = []
 
 //Criar dados / ususarios / fazer requeisição POST
-app.post('/users', (req, res) => {
-    console.log(req.body)
+
+
+app.post('/users', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email
+            age: req.body.age
+        }
+    })
+
+    users.push(req.body)
     res.status(201).json(users);
 })
 
@@ -18,5 +75,5 @@ app.get('/users', (req, res) => {
     res.status(200).json(users);
 });
 
-//ip 127.0.0.1:8080*/
-app.listen(8082);
+//ip 127.0.0.1:8082*/
+//app.listen(8082); 
