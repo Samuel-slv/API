f3da34d1b387f1fd00f6b30394e8ab0e86297d17
Samuel da Silva <samuelsilva2017ramos@gmail.com>
2025-04-04 11:50:14 -0300

    Atualização de JS,novo banco.

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-
 const app = express();
 app.use(express.json());
-const users = []
 
 //Criar dados / ususarios / fazer requeisição POST
 app.post('/users', async (req, res) => {
-
     await prisma.user.create({
         data: {
             name: req.body.name,
@@ -18,62 +15,58 @@ app.post('/users', async (req, res) => {
             age: req.body.age
         }
     })
-
-    users.push(req.body);
-    res.status(201).json(users);
+    res.status(201).json(req.body);
 })
 
 //Mostrar /dados / usuarios / fazer requeisição GET
 
 app.get('/users', async (req, res) => {
 
-    const users = prisma.user.findMany();
-    res.status(200).json(users);
+    let users = [];
+
+    if (req.query) { 
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name
+            }
+        })
+        return res.status(200).json(users);
+    }else{
+
+        const users = await prisma.user.findMany();
+        res.status(200).json(users);
+    }
 });
+
+
+//Deletar dados dados / ususarios / fazer requeisição PUT
+app.put('/users/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+
+    })
+    res.status(201).json({ messege: 'Usuario atualizado com sucesso!' });
+})
+
+//Deletar dados dados 44 / ususarios / fazer requeisição DELETE 
+app.put('/users/:id', async (req, res) => {
+
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        },
+        
+    })
+    res.status(201).json({ messege: 'Usuario atualizado com sucesso!' });
+})
 
 //ip 127.0.0.1:8082
 app.listen(8082);
-
-
-
-
-
-
-
-
-
-/* import express from 'express';
-import {PrismaClient} from '@prisma/client';
-
-const prisma = new PrismaClient();
-const app = express();
-app.use(express.json());
-
-const users = []
-
-//Criar dados / ususarios / fazer requeisição POST
-
-
-app.post('/users', async (req, res) => {
-
-    await prisma.user.create({
-        data: {
-            name: req.body.name,
-            email: req.body.email
-            age: req.body.age
-        }
-    })
-
-    users.push(req.body)
-    res.status(201).json(users);
-})
-
-//Mostrar /dados / usuarios / fazer requeisição GET
-
-app.get('/users', (req, res) => {
-    //res.status(200).json(users);
-    res.status(200).json(users);
-});
-
-//ip 127.0.0.1:8082*/
-//app.listen(8082); 
